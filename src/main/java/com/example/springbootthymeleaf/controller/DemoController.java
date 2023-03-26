6b12291c83189ac0e54db4decc31e1893eed2e45
xye804 <xye804@aucklanduni.ac.nz>
2023-03-26 19:26:16 +0800

    Update back-end code segment

--- src/main/java/com/example/springbootthymeleaf/controller/DemoController.java
+++ src/main/java/com/example/springbootthymeleaf/controller/DemoController.java
@@ -4,6 +4,9 @@ import com.example.springbootthymeleaf.pojo.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
 
 @Controller
 public class DemoController {
@@ -13,5 +16,13 @@ public class DemoController {
         User user = new User("sbzjy", 100);
         model.addAttribute("user", user);
         return "index";
+    }
+
+    @GetMapping("/")
+    public ModelAndView index() {
+        ArrayList<String> names = new ArrayList<>();
+        ModelAndView modelAndView = new ModelAndView("index");
+        modelAndView.addObject("names", names);
+        return modelAndView;
     }
 }
