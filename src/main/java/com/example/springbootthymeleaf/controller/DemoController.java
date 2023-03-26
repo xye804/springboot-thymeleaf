fe9fdc521ddc96eec170362651017e42d681f6a2
xye804 <xye804@aucklanduni.ac.nz>
2023-03-26 18:34:26 +0800

    Update back-end code segment

---
+++ src/main/java/com/example/springbootthymeleaf/controller/DemoController.java
@@ -0,0 +1,17 @@
+package com.example.springbootthymeleaf.controller;
+
+import com.example.springbootthymeleaf.pojo.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController {
+
+    @GetMapping("/")
+    public String index(Model model) {
+        User user = new User("sbzjy", 100);
+        model.addAttribute("user", user);
+        return "th-text";
+    }
+}
---
+++ src/main/java/com/example/springbootthymeleaf/pojo/User.java
@@ -0,0 +1,15 @@
+package com.example.springbootthymeleaf.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    private String username;
+    private Integer age;
+
+}
